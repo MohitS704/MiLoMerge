1e7394b0f3b0447fb659cbd6509f582410e4c607
Mohit Srivastav <vir.moh@gmail.com>
2023-09-11 18:15:42 -0400

    stuff

--- brunelle_merger/brunelle_merger.py
+++ brunelle_merger/brunelle_merger.py
@@ -1,92 +1,182 @@
 import numpy as np
-import scipy.stats as sp
 
-def split_hist(counts, bins):
-    median = len(bins)//2
+# def split_hist(counts, bins):
+#     median = len(bins)//2
     
-    split_up = (counts[:median], bins[:median+1]), (counts[median:], bins[median:])
-    return split_up
+#     split_up = (counts[:median], bins[:median+1]), (counts[median:], bins[median:])
+#     return split_up
 
-def combine_hist(left_counts, left_bins, right_counts, right_bins):
-    if left_bins[-1] != right_bins[0]:
-        print(left_bins, right_bins)
-        raise ValueError("Bins must be continuous!")
+# def combine_hist(left_counts, left_bins, right_counts, right_bins):
+#     if left_bins[-1] != right_bins[0]:
+#         print(left_bins, right_bins)
+#         raise ValueError("Bins must be continuous!")
     
-    return np.concatenate((left_counts, right_counts)), np.concatenate((left_bins, right_bins[1:]))
+#     return np.concatenate((left_counts, right_counts)), np.concatenate((left_bins, right_bins[1:]))
 
-def merge_bin_on_edge(left_counts, left_bins, right_counts, right_bins):
-    if left_bins[-1] != right_bins[0]:
-        print(left_bins, right_bins)
-        raise ValueError("Bins must be continuous!")
+# def merge_bin_on_edge(left_counts, left_bins, right_counts, right_bins):
+#     if left_bins[-1] != right_bins[0]:
+#         print(left_bins, right_bins)
+#         raise ValueError("Bins must be continuous!")
         
-    merged = (np.concatenate( (left_counts[:-1], [left_counts[-1] + right_counts[0]], right_counts[1:]) ), 
-            np.concatenate( (left_bins[:-1], right_bins[1:]) ) )
+#     merged = (np.concatenate( (left_counts[:-1], [left_counts[-1] + right_counts[0]], right_counts[1:]) ), 
+#             np.concatenate( (left_bins[:-1], right_bins[1:]) ) )
     
-    return merged
+#     return merged
 
 def merge_ahead(counts, bins, i):
-    counts[i]
+    """Merges the bin at index i with its buddy at index i+1
+    The buddy absorbs the selected bin
+    i.e. [2,3,4] -> [2,4] if i=1
 
-class Brunelle_merger(object):
+    Parameters
+    ----------
+    counts : numpy.ndarray
+        the counts for your histogram
+    bins : numpy.ndarray
+        the bins for your histogram
+    i : int
+        the index
+
+    Returns
+    -------
+    Tuple(numpy.ndarray, numpy.ndarray)
+        the counts and bins for your merged histogram (numpy style histogram)
+
+    Raises
+    ------
+    ValueError
+        if you merge an invalid point
+    """
+    if i+1 >= len(counts) or i == 0:
+        raise ValueError("YOU IDIOT WHY WOULD YOU MERGE ON THE EDGES THAT LOSES THE RANGE")
+    
+    counts = np.concatenate( (counts[:i], [counts[i] + counts[i+1]], counts[i+2:]) )
+    #above does the following, <rest_of_list> + (merged_counts) + <rest_of_list>
+    bins = np.concatenate( (bins[:i], bins[i+1:]) )
+    #merge bins by removing a bin edge at index i
+    return counts, bins
+
+class Brunelle_merger(object): #Professor Nathan Brunelle!
+    #https://engineering.virginia.edu/faculty/nathan-brunelle
     def __init__(self, counts_1, counts_2, bins, distanceFunc) -> None:
+        """Our Funky Merger (Name will be changed later)
+
+        Parameters
+        ----------
+        counts_1 : numpy.ndarray
+            histogram counts for hypothesis 1
+        counts_2 : numpy.ndarray
+            histogram counts for hypothesis 2
+        bins : numpy.ndarray
+            histogram bins for both hypotheses
+        distanceFunc : function
+            a function to determine distances between two sets of counts
+
+        Raises
+        ------
+        TypeError
+            distanceFunc must be a function that takes in two parameters (two counts)
+        ValueError
+            If your lengths are wrong
+        """
+        
+        if len(counts_1) != len(counts_2):
+            raise ValueError("Count lengths are not the same! {:.0f} != {:.0f}".format(len(counts_1), len(counts_2)))
+        
+        if len(counts_1) != len(bins) - 1:
+            raise ValueError("Invalid lengths! {:.0f} != {:.0f} + 1".format(len(counts_1), len(bins)))
+        
+        if len(counts_2) != len(bins) - 1:
+            raise ValueError("Invalid lengths! {:.0f} != {:.0f} + 1".format(len(counts_2), len(bins)))
+        
         self.counts_1 = counts_1
         self.counts_2 = counts_2
         
         self.original_bins = bins
         
-        if not isinstance(distanceFunc, function):
+        if not callable(distanceFunc):
             raise TypeError("Function provided must be of type function, not type {}!".format(type(distanceFunc)))
         
         self.distanceFunc = distanceFunc
         
         self.scores = {}
 
-    def compare(self, counts_left_1, counts_left_2, bins_left, counts_right_1, counts_right_2, bins_right):
-        merged_1, merged_bins = merge_bin_on_edge(counts_left_1, bins_left, counts_right_1 , bins_right)
-        merged_2, _ = merge_bin_on_edge(counts_left_2, bins_left, counts_right_2, bins_right)
+    # def compare(self, counts_left_1, counts_left_2, bins_left, counts_right_1, counts_right_2, bins_right): OLD DO NOT USE
+    #     merged_1, merged_bins = merge_bin_on_edge(counts_left_1, bins_left, counts_right_1 , bins_right)
+    #     merged_2, _ = merge_bin_on_edge(counts_left_2, bins_left, counts_right_2, bins_right)
         
-        unmerged_1, unmerged_bins = combine_hist(counts_left_1, bins_left, counts_right_1, bins_right)
-        unmerged_2, _ = combine_hist(counts_left_2, bins_left, counts_right_2, bins_right)
+    #     unmerged_1, unmerged_bins = combine_hist(counts_left_1, bins_left, counts_right_1, bins_right)
+    #     unmerged_2, _ = combine_hist(counts_left_2, bins_left, counts_right_2, bins_right)
         
-        T = self.distanceFunc(unmerged_1, unmerged_2) - self.distanceFunc(merged_1, merged_2)
-        # print("USING:", counts_left_1, counts_left_2, bins_left, "&", counts_right_1, counts_right_2, bins_right)
+    #     T = self.distanceFunc(unmerged_1, unmerged_2) - self.distanceFunc(merged_1, merged_2)
+    #     # print("USING:", counts_left_1, counts_left_2, bins_left, "&", counts_right_1, counts_right_2, bins_right)
         
-        if T:
-            # print("MERGED:", merged_1, merged_2, merged_bins)
-            return merged_1, merged_2, merged_bins
-        else:
-            # print("REMAINED:", unmerged_1, unmerged_2, unmerged_bins)
-            return unmerged_1, unmerged_2, unmerged_bins
+    #     if T:
+    #         # print("MERGED:", merged_1, merged_2, merged_bins)
+    #         return merged_1, merged_2, merged_bins
+    #     else:
+    #         # print("REMAINED:", unmerged_1, unmerged_2, unmerged_bins)
+    #         return unmerged_1, unmerged_2, unmerged_bins
 
 
-    def brunelle_merge(self, counts_1, counts_2, bins):
-        if len(bins) < 2:
-            raise ValueError("Cannot have fewer than 2 bins!")
-        if len(bins) == 2:
-            # print("BASECASE", counts_1, counts_2, bins)
-            return counts_1, counts_2, bins
+    # def brunelle_merge(self, counts_1, counts_2, bins): OLD DO NOT USE
+    #     if len(bins) < 2:
+    #         raise ValueError("Cannot have fewer than 2 bins!")
+    #     if len(bins) == 2:
+    #         # print("BASECASE", counts_1, counts_2, bins)
+    #         return counts_1, counts_2, bins
         
-        # print("ITERATION:", counts_1, counts_2, bins)
+    #     # print("ITERATION:", counts_1, counts_2, bins)
         
-        (counts_left_1, bins_left), (counts_right_1, bins_right) = split_hist(counts_1, bins)
-        (counts_left_2, _), (counts_right_2, _) = split_hist(counts_2, bins)
+    #     (counts_left_1, bins_left), (counts_right_1, bins_right) = split_hist(counts_1, bins)
+    #     (counts_left_2, _), (counts_right_2, _) = split_hist(counts_2, bins)
         
-        counts_left_1, counts_left_2, bins_left = self.brunelle_merge(counts_left_1, counts_left_2, bins_left)
-        counts_right_1, counts_right_2, bins_right = self.brunelle_merge(counts_right_1, counts_right_2, bins_right)
+    #     counts_left_1, counts_left_2, bins_left = self.brunelle_merge(counts_left_1, counts_left_2, bins_left)
+    #     counts_right_1, counts_right_2, bins_right = self.brunelle_merge(counts_right_1, counts_right_2, bins_right)
         
-        # print("ENTERING COMPARE:", counts_left_1, counts_left_2, bins_left, "&", counts_right_1, counts_right_2, bins_right)
+    #     # print("ENTERING COMPARE:", counts_left_1, counts_left_2, bins_left, "&", counts_right_1, counts_right_2, bins_right)
         
-        return self.compare(counts_left_1, counts_left_2, bins_left, counts_right_1, counts_right_2, bins_right)
+    #     return self.compare(counts_left_1, counts_left_2, bins_left, counts_right_1, counts_right_2, bins_right)
     
     def greedy_merge(self, target_bin_number):
-        new_counts_1 = self.counts_1
-        new_counts_2 = self.counts_2
-        new_bins = self.original_bins
-        
-        
+        """merges greedily. You can never dig too deep!
+
+        Parameters
+        ----------
+        target_bin_number : int
+            The target number of bins you want
+
+        Returns
+        -------
+        Tuple(numpy.ndarray, numpy.ndarray, numpy.ndarray)
+            a tuple of the new counts (1 and 2) and the new bins
+        """
+        new_counts_1 = self.counts_1.copy()
+        new_counts_2 = self.counts_2.copy()
+        new_bins = self.original_bins.copy()
         
         while len(new_bins) > target_bin_number:
+            current_distance = self.distanceFunc(new_counts_1, new_counts_2)
             
+            combinations = {}
+            scores = {}
+            
+            for i in range(1, len(new_counts_1) - 1): #don't merge edge bins/counts!
+                temp_counts_1, temp_bins = merge_ahead(new_counts_1, new_bins, i)
+                temp_counts_2, _ = merge_ahead(new_counts_2, new_bins, i)
+                
+                score = current_distance - self.distanceFunc(temp_counts_1, temp_counts_2)
+                
+                combinations[i] = (temp_counts_1, temp_counts_2, temp_bins)
+                scores[i] = score
+                
+            new_counts_1, new_counts_2, new_bins = combinations[ min(scores, key=scores.get) ]
+            # print(combinations.keys())
+            # new_counts_1, new_counts_2, new_bins = combinations[
+            #     np.min(list(combinations.keys()))
+            #     ]
+        
+        return new_counts_1, new_counts_2, new_bins
     
 
 
--- tester.py
+++ tester.py
@@ -1,77 +1,275 @@
 import brunelle_merger.brunelle_merger as bm
 import numpy as np
-import sys
 import matplotlib.pyplot as plt
-sys.path.append("/eos/home-m/msrivast/CMSSW_12_2_0_/src/HexUtils/")
-import useful_helpers as help
-import uproot
+import scipy.stats as sp
+import mplhep as hep
 
-g1_discr = uproot.open("/eos/home-m/msrivast/CMSSW_12_2_0_/src/HexUtils/g1discrs.root")
-g4_discr = uproot.open("/eos/home-m/msrivast/CMSSW_12_2_0_/src/HexUtils/g4discrs.root")
+data_1 = np.random.normal(3, 2, 1000)
+data_2 = np.random.normal(5, 5, 1000)
 
+counts, bins = np.histogram(data_1, 50, range=[0,10], density=True)
+counts /= counts.sum()
 
-g1_data = {
-    'd0-':[]
-}
+counts_2, _ = np.histogram(data_2, bins, density=True)
+counts_2 /= counts_2.sum()
 
-g4_data = {
-    'd0-':[]
-}
+# print(counts, counts_2, bins)
 
-for key in g1_discr.keys():
-    g1_data['d0-'] += list(g1_discr[key]['d0-'].array(library='np'))
-    g4_data['d0-'] += list(g4_discr[key]['d0-'].array(library='np'))
+# merging = 1
 
-initial_bins = np.linspace(0,1,200)
-
-initial_data_1 = g1_data['d0-']
-
-initial_counts_1, _ = np.histogram(initial_data_1, initial_bins, density=True)
-
-initial_data_2 = g4_data['d0-']
-
-initial_counts_2, _ = np.histogram(initial_data_1, initial_bins, density=True)
-
-plt.hist(initial_data_1, initial_bins, histtype='step', density=True)
-plt.hist(initial_data_2, initial_bins, histtype='step', density=True)
-plt.savefig('testing_hist.png')
-
+# print("Merging bin {:.0f} with bin {:.0f}".format(merging, merging+1))
+# print(bm.merge_ahead(counts, bins, merging))
 
 def earth_mover(counts_1, counts_2):
+    """A stupid wrapper for the wasserstein/earth mover's distance
+
+    Parameters
+    ----------
+    counts_1 : numpy.ndarray
+        counts for hypo 1
+    counts_2 : numpy.ndarray
+        counts for hypo 2
+
+    Returns
+    -------
+    float
+        a distance!
+    """
     return sp.wasserstein_distance(counts_1, counts_2)
 
 def heshy_metric(counts_1, counts_2):
-        
+    """A stupid wrapper for heshy's distance (calculates a ROC curve)
+
+    Parameters
+    ----------
+    counts_1 : numpy.ndarray
+        counts for hypo 1
+    counts_2 : numpy.ndarray
+        counts for hypo 2
+
+    Returns
+    -------
+    float
+        a distance!
+    """
     sorted_ratios = sorted(
         list(enumerate(counts_1/counts_2)), key=lambda x: x[1], reverse=True
     )
-    sorted_ratios = np.array(sorted_ratios)
     
-    sorted_counts = sorted_ratios[:,1]
     
-    cumsum = np.cumsum(sorted_counts)
+    sorted_ratios = np.array(sorted_ratios)[:,0].astype(int) #gets the bin indices only for the ordered ratio pairs
+    sorted_ratios[~np.isfinite(sorted_ratios)] = 0
     
-    return np.trapz(cumsum, range(1, len(cumsum) + 1))
+    length = len(sorted_ratios) + 1
     
-def new_metric(counts_1, counts_2):
-    return counts_1/counts_2
+    PAC = np.zeros(length) #"positive" above cutoff
+    PBC = np.zeros(length) #"positive" below cutoff
+    NAC = np.zeros(length) #"negative" above cutoff
+    NBC = np.zeros(length) #"negative" below cutoff
+    
+    
+    for n in range(length):
+        above_cutoff = sorted_ratios[n:]
+        below_cutoff = sorted_ratios[:n]
+        
+        PAC[n] = counts_1[above_cutoff].sum() #gets the indices listed
+        PBC[n] = counts_1[below_cutoff].sum()
+        
+        NAC[n] = counts_2[above_cutoff].sum()
+        NBC[n] = counts_2[below_cutoff].sum()
+    
+    TPR = 1 - PAC/(PAC + PBC) #vectorized calculation
+    FPR = 1 - NAC/(NAC + NBC)
+    
+    TPR[~np.isfinite(TPR)] = 0
+    FPR[~np.isfinite(FPR)] = 0
+    
+    return np.trapz(TPR, FPR)
+    
+def frown_metric(counts_1, counts_2):
+    """A stupid wrapper for the ratio/frowning metric
+
+    Parameters
+    ----------
+    counts_1 : numpy.ndarray
+        counts for hypo 1
+    counts_2 : numpy.ndarray
+        counts for hypo 2
+
+    Returns
+    -------
+    float
+        a distance!
+    """
+    return np.sum(counts_1/counts_2)
+
+def clown_metric(counts_1, counts_2):
+    """A stupid wrapper for the enhanced ratio/clown metric
+
+    Parameters
+    ----------
+    counts_1 : numpy.ndarray
+        counts for hypo 1
+    counts_2 : numpy.ndarray
+        counts for hypo 2
+
+    Returns
+    -------
+    float
+        a distance!
+    """
+    return np.sum( np.abs(counts_1 - counts_1)/(counts_1 + counts_2) )
+
+def ROC_curve(sample1, sample2, bins=100, lower=0, upper=1):
+    """This function produces a ROC curve from an attribute like phi, cos(theta1), D_{0-}, etc.
+
+    Parameters
+    ----------
+    sample1 : numpy.ndarray
+        The first data sample for your attribute. This is your "True" data
+    sample2 : numpy.ndarray
+        The second data sample for your attribute. This if your "False" data
+    bins : int or numpy.ndarray, optional
+        The number of bins for the ROC calculation. Can also be given a list of bins., by default 100
+    lower : float
+        The lower end of your sample range
+    upper : float
+        The upper end of your sample range
+    
+
+    Returns
+    -------
+    tuple(numpy.ndarray, numpy.ndarray, float)
+        returns the true rate, the false rate, and the area under the curve (assuming true rate is the x value)
+    """
+    
+    sample1 = np.array(sample1)
+    sample2 = np.array(sample2)
+    
+    if isinstance(bins, int):
+        _, bins = np.histogram([], bins=bins, range=[lower, upper])
+    
+    hypo2_counts, bins = np.histogram(sample2, bins=bins, density=True)
+    hypo2_counts /= hypo2_counts.sum()
+    
+    hypo1_counts, _ = np.histogram(sample1, bins=bins, density=True)
+    hypo1_counts /= hypo1_counts.sum()
+    
+    # print(list(g1_phi_counts))
+    # print()
+    # print(list(g4_phi_counts))
+    
+    ratios = sorted(
+        list(enumerate(hypo1_counts/hypo2_counts)), key=lambda x: x[1], reverse=True
+    )
+    # print(ratios)
+    
+    ratios = np.array(ratios)[:,0].astype(int) #gets the bin indices only for the ordered ratio pairs
+    ratios[~np.isfinite(ratios)] = 0
+    # print(ratios)
+    # print()
+    length = len(ratios) + 1
+    
+    PAC = np.zeros(length) #"positive" above cutoff
+    PBC = np.zeros(length) #"positive" below cutoff
+    NAC = np.zeros(length) #"negative" above cutoff
+    NBC = np.zeros(length) #"negative" below cutoff
+    
+    
+    for n in range(length):
+        above_cutoff = ratios[n:]
+        below_cutoff = ratios[:n]
+        
+        PAC[n] = hypo1_counts[above_cutoff].sum() #gets the indices listed
+        PBC[n] = hypo1_counts[below_cutoff].sum()
+        
+        NAC[n] = hypo2_counts[above_cutoff].sum()
+        NBC[n] = hypo2_counts[below_cutoff].sum()
+        
+        # for bin_index in above_cutoff: #The above lines are the same as this commented code but vectorized
+        #     PAC += g1_phi_counts[bin_index]
+        #     NAC += g4_phi_counts[bin_index]
+        
+        # for bin_index in below_cutoff:
+        #     PBC += g1_phi_counts[bin_index]
+        #     NBC += g4_phi_counts[bin_index]
+        # TPR.append(1 - PAC/(PAC + PBC))
+        # FPR.append(1 - NAC/(NAC + NBC))
+        
+        
+    TPR = 1 - PAC/(PAC + PBC) #vectorized calculation
+    FPR = 1 - NAC/(NAC + NBC)
+    
+    TPR[~np.isfinite(TPR)] = 0
+    FPR[~np.isfinite(FPR)] = 0
+    
+    return TPR, FPR, np.trapz(TPR, FPR)
 
 
-_, _, area_orig = help.ROC_curve(initial_data_1, initial_data_2, initial_bins)
+print( "OG Score:", ROC_curve(data_1.copy(), data_2.copy(), bins.copy() )[-1] )
 
-x_w = bm.brunelle_merge(initial_counts_1, initial_counts_2, initial_bins, earth_mover)
+edm = bm.Brunelle_merger(counts, counts_2, bins, earth_mover)
+edm_terms = edm.greedy_merge(5)
 
-print("DONE!")
+print(*edm_terms)
+print("EMD Score:", ROC_curve(data_1, data_2, edm_terms[-1], 0, 10)[-1] )
 
-_, _, area = help.ROC_curve(initial_data_1, initial_data_2, x_w[-1])
 
-x_h = bm.brunelle_merge(initial_counts_1, initial_counts_2, initial_bins, heshy_metric)
+heshy = bm.Brunelle_merger(counts, counts_2, bins, heshy_metric)
+heshy_terms = heshy.greedy_merge(5)
 
-print("DONE!")
+print(*heshy_terms)
+print("HESHY Score:", ROC_curve(data_1, data_2, heshy_terms[-1], 0, 10)[-1] )
 
-_, _, area_2 = help.ROC_curve(initial_data_1, initial_data_2, x_h[-1])
+frown = bm.Brunelle_merger(counts, counts_2, bins, frown_metric)
+frown_terms = frown.greedy_merge(5)
 
-# print(x_w)
-# print(x_h)
+print(*frown_terms)
+print("FROWN SCORE:", ROC_curve(data_1, data_2, frown_terms[-1], 0, 10)[-1])
 
-print(area_orig, area, area_2)
+clown = bm.Brunelle_merger(counts, counts_2, bins, clown_metric)
+clown_terms = clown.greedy_merge(5)
+
+print(*clown_terms)
+print("CLOWN SCORE:", ROC_curve(data_1, data_2, clown_terms[-1], 0, 10)[-1])
+
+
+
+mosaic = [[1,2,3],
+          [1,4,5]]
+
+ref_dict = {
+    1 : bins,
+    2 : edm_terms[-1],
+    3 : heshy_terms[-1],
+    4 : frown_terms[-1],
+    5 : clown_terms[-1]
+}
+
+ref_names = {
+    1 : "OG",
+    2 : "EMD",
+    3 : "HESHY",
+    4 : "FROWN",
+    5 : "CLOWN"
+}
+
+
+fig, ax = plt.subplot_mosaic(mosaic, figsize=(10,7))
+
+for i in ref_dict.keys():
+    counts_1, _ = np.histogram(data_1, ref_dict[i], density=True)
+    counts_1 /= counts_1.sum()
+    counts_2, _ = np.histogram(data_2, ref_dict[i], density=True)
+    counts_2 /= counts_2.sum()
+    
+    hep.histplot(counts_1, ref_dict[i], label="1", lw=3, ax=ax[i])
+    hep.histplot(counts_2, ref_dict[i], label="2", lw=3, ax=ax[i])
+    
+    ax[i].set_title(ref_names[i] + " SCORE={:.2f}".format(ROC_curve(data_1.copy(), data_2.copy(), ref_dict[i].copy() )[-1]))
+    ax[i].legend()
+
+
+fig.suptitle("Reducing LHS to 5 bins")
+fig.tight_layout()
+fig.savefig('testing_hist.png')
