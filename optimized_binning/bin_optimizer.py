04f0db71311688d5e6469ba660c67944f76e0e95
Mohit Srivastav <vir.moh@gmail.com>
2024-01-20 07:47:32 +0100

    I am speed

--- optimized_binning/bin_optimizer.py
+++ optimized_binning/bin_optimizer.py
@@ -36,9 +36,11 @@ class Merger():
         else:
             weights = np.array(weights)
 
+        self._merger_type = None
+
         self.weights = np.outer(weights, weights)
         self.n_hypotheses = len(counts)
-        self.n_items = len(counts[0])
+        self.n_items = self.original_n_items = len(counts[0])
         #self.n is the number of hypotheses, self.n_items is the current number of bin_edges
 
         self.comp_to_first = comp_to_first
@@ -49,28 +51,46 @@ class Merger():
         self.bin_edges = np.array(bin_edges)
 
     @staticmethod
-    @nb.njit(fastmath=True)
-    def _mlm(n, counts, weights, b, b_prime, comp_to_first):
+    @nb.njit(fastmath=True, cache=True)
+    def __mlm_driver_comp_to_first(n, counts, weights, b, b_prime):
 
         metric_val = 0
-        if comp_to_first:
-            initial_range = np.arange(0)
-        else:
-            initial_range = np.arange(n)
+        for h_prime in nb.prange(h+1, n):
+            t1, t2 = counts[h][b]*weights[h][h_prime], counts[h_prime][b_prime]*weights[h][h_prime]
+            t3, t4 = counts[h][b_prime]*weights[h][h_prime], counts[h_prime][b]*weights[h][h_prime] 
 
-        for h in initial_range:
-            for h_prime in nb.prange(h+1, n):
-
-                mat = np.array([
-                    [counts[h][b], counts[h][b_prime]],
-                    [counts[h_prime][b], counts[h_prime][b_prime]]
-                ], dtype=np.float64)
-
-                mat *= weights[h][h_prime]
-
-                metric_val += (mat[0][0]*mat[1][1])**2 + (mat[0][1]*mat[1][0])**2 - 2*np.prod(mat)
+            metric_val += (t1*t2)**2 + (t3*t4)**2 - 2*t1*t2*t3*t4
 
         return metric_val
+
+    @staticmethod
+    @nb.njit(fastmath=True, cache=True)
+    def __mlm_driver_comp_to_all(n, counts, weights, b, b_prime):
+
+        metric_val = 0
+        for h in np.arange(n):
+            for h_prime in nb.prange(h+1, n):
+                t1, t2 = counts[h][b]*weights[h][h_prime], counts[h_prime][b_prime]*weights[h][h_prime]
+                t3, t4 = counts[h][b_prime]*weights[h][h_prime], counts[h_prime][b]*weights[h][h_prime] 
+
+                metric_val += (t1*t2)**2 + (t3*t4)**2 - 2*t1*t2*t3*t4
+
+        return metric_val
+
+    def _mlm(self, b, b_prime):
+        if self.comp_to_first:
+            return self.__mlm_driver_comp_to_first(
+                self.n_hypotheses, self.counts,
+                self.weights, b, b_prime
+            )
+
+        return self.__mlm_driver_comp_to_all(
+            self.n_hypotheses, self.counts,
+            self.weights, b, b_prime
+        )
+
+    def __repr__(self) -> str:
+        return f"Merger of type {self._merger_type} merging {self.original_n_items}"
 
 class MergerLocal(Merger):
     def __init__(
@@ -83,6 +103,8 @@ class MergerLocal(Merger):
         super().__init__(bin_edges, *counts, weights=weights, comp_to_first=comp_to_first)
         if len(self.bin_edges.shape) > 1:
             raise ValueError("LOCAL MERGING CAN ONLY HANDLE 1-DIMENSIONAL ARRAYS")
+
+        self._merger_type = "Local"
 
     def _merge(self, i, j):
         if i == j + 1:
@@ -132,18 +154,12 @@ class MergerLocal(Merger):
             scores = {}
 
             for i in range(1, self.n_items - 1):
-                score = self._mlm(
-                    self.n_hypotheses, self.counts,
-                    self.weights, i, i+1, self.comp_to_first
-                    )
+                score = self._mlm(i, i+1)
 
                 combinations[i] = (i, i+1)
                 scores[i] = score
 
-            score = self._mlm(
-                self.n_hypotheses, self.counts, self.weights,
-                1, 0, self.comp_to_first
-                )
+            score = self._mlm(1, 0)
             #try merging counts 1 with count 0 (backwards)
             combinations[0] = (1, 0)
             scores[0] = score
@@ -179,6 +195,8 @@ class MergerNonlocal(Merger):
             weights=weights, comp_to_first=comp_to_first
             )
 
+        self._merger_type = "Non-local"
+
         self.physical_bins = da.array(bin_edges)
 
         if len(self.physical_bins.shape) > 1:
@@ -187,38 +205,45 @@ class MergerNonlocal(Merger):
             self.n_observables = 1
 
         self.__cur_iteration_tracker = {}
-        self.scores = da.zeros((self.n_items, self.n_items), dtype=np.float64)
+        self.scores = np.zeros((self.n_items, self.n_items), dtype=np.float64)
+        self.things_to_recalculate = tuple(range(self.n_items))
 
 
     @staticmethod
     @nb.njit
     def _merge(n_orig, n, counts, score_matrix, i, j):
-        merged_counts = da.zeros(shape=(n_orig, n - 1), dtype=np.float64)
+        merged_counts = np.zeros(shape=(n_orig, n - 1), dtype=np.float64)
 
-        k = 0
-        for c in nb.prange(n):
-            if c not in (i, j):
-                merged_counts[:, k] = counts[:, c]
-                score_matrix[:, k], score_matrix[k] = score_matrix[:, n], score_matrix[n]
+        # k = 0
+        # for c in nb.prange(n):
+        #     if c not in (i, j):
+        #         merged_counts[:, k] = counts[:, c]
+        #         score_matrix[:, k], score_matrix[k] = score_matrix[:, n], score_matrix[n]
 
-                k += 1
-        merged_counts[:, k] = counts[:, i] + counts[:, k]
-        score_matrix[k:] = score_matrix[:, k:] = np.inf
+        #         k += 1
+        merged_counts[:, :-1] = np.delete(counts, [i,j])
+        merged_counts[:, -1] = counts[:, i] + counts[:, j]
+
+        score_matrix = np.delete(np.delete(score_matrix, [i, j], axis=0), [i, j], axis=1)
+        # score_matrix[k:] = score_matrix[:, k:] = np.inf
 
         return counts, score_matrix, k
 
     @staticmethod
     @nb.njit
-    def _closest_pair(n, counts, weights, things_to_recalculate, score_matrix, comp_to_first):
+    def __closest_pair_driver(n, counts, weights, things_to_recalculate, score_matrix, comp_to_first):
         for i in nb.prange(n):
             for j in nb.prange(things_to_recalculate):
                 if i == j:
                     score_matrix[i][j] = np.inf
                     continue
                 score_matrix[i][j] = Merger._mlm(n, counts, weights, i, j, comp_to_first)
+        return score_matrix
 
-        smallest_distance_index = np.unravel_index(score_matrix.argmin(), score_matrix.shape)
-        smallest_distance = score_matrix[smallest_distance_index]
+    def _closest_pair(self):
+        self.scores = self.__closest_pair_driver(self.n_items, self.counts, self.weights, self.things_to_recalculate, self.scores, self.comp_to_first)
+        smallest_distance_index = np.unravel_index(self.scores.argmin(), self.scores.shape)
+        smallest_distance = self.scores[smallest_distance_index]
 
         if not np.isfinite(smallest_distance):
             raise ValueError("Distance function has produced nan/inf at some point")
